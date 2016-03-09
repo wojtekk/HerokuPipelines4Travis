d055a090e98d45a83f978ae909a73ade79920760
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2016-03-09 15:43:35 +0100

    Add export script

--- src/heroku/export.js
+++ src/heroku/export.js
@@ -1,11 +1,25 @@
-var heroin = require('heroin-js');
+'use strict';
 
-var configurator = heroin(process.env.HEROKU_API_KEY, {debug: false});
+const heroin = require('heroin-js');
 
-configurator.export('').then(function (result) {
-  console.log(result);
-});
+if (!process.argv[2]) {
+    console.error('App name is empty');
+    return 1;
+}
+let appName = process.argv[2];
+let herokuApiKey;
+if (process.argv[3]) {
+    herokuApiKey = process.argv[3];
+} else if (process.env.HEROKU_API_KEY) {
+    herokuApiKey = process.env.HEROKU_API_KEY;
+}
+if (!herokuApiKey) {
+    console.error('Heroku API key is empty');
+    return 2;
+}
 
-configurator.export('').then(function (result) {
-  console.log(result);
+let configurator = heroin(herokuApiKey, {debug: false});
+
+configurator.export(appName).then(function (result) {
+    console.log(result);
 });
---
+++ src/heroku/export.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#HP4T_EXTENSION_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../herokupipelines4travis/" && pwd )"
+
+#node $HP4T_EXTENSION_DIR/src/heroku/export.js $1
+
+node ./src/heroku/export.js $1
