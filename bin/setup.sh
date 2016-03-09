d052630d97b4da9775c3981b3d961348ebc46609
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2016-03-09 13:21:16 +0100

    Script reorganisation - other approach, without postinstall  scripts

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,7 +5,5 @@ export HP4T_EXTENSION_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )
 
 . $HP4T_EXTENSION_DIR/scripts/variables.sh
 
-$HP4T_SCRIPTS_DIR/make-link.sh
 $HP4T_SCRIPTS_DIR/initialise-infrastructure.sh
 $HP4T_SCRIPTS_DIR/initialise-travis.sh
-$HP4T_SCRIPTS_DIR/add-rules-to-gitignore.sh
--- scripts/add-rules-to-gitignore.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "scripts/hp4t/" >> $HP4T_PROJECT_DIR/.gitignore
