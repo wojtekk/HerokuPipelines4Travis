fe06c90e5e9a05257a851485075487994cbc51a5
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2016-03-09 15:47:14 +0100

    Fix export script

--- src/heroku/export.sh
+++ src/heroku/export.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#HP4T_EXTENSION_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../herokupipelines4travis/" && pwd )"
+HP4T_EXTENSION_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../herokupipelines4travis/" && pwd )"
 
-#node $HP4T_EXTENSION_DIR/src/heroku/export.js $1
-
-node ./src/heroku/export.js $1
+node $HP4T_EXTENSION_DIR/src/heroku/export.js $1
