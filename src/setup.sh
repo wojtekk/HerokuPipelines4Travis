951df08b10933e219dd145da77030290b3974b1d
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2016-03-21 22:52:13 +0100

    Add project type recognition to setup

--- src/setup.sh
+++ src/setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+projectType=$1
+
+if ! [ "$projectType" == "nodejs" ] && ! [ "$projectType" == "php" ] ; then
+ echo "Unknown project type."
+ exit 3;
+fi
+
 # Initialise infrastructure (HeroinJS)
 
 echo "[HP4T] Infrastructure configration: initialising"
@@ -8,7 +15,7 @@ if [ -d $HP4T_PROJECT_DIR/infrastructure ]; then
   echo "[HP4T] Infrastructure configration: exists, moving directory `infrastructure` to `infrastructure.pre_hp4t`."
   mv $HP4T_PROJECT_DIR/infrastructure $HP4T_PROJECT_DIR/infrastructure.pre_hp4t
 fi
-cp -r $HP4T_TEMPLATES_DIR/infrastructure/npm $HP4T_PROJECT_DIR/infrastructure
+cp -r $HP4T_TEMPLATES_DIR/infrastructure/$projectType $HP4T_PROJECT_DIR/infrastructure
 
 # Initialise Travis
 
@@ -17,4 +24,4 @@ if [ -f $HP4T_PROJECT_DIR/.travis.yml ]; then
   echo "[HP4T] Travis configuration: exists, moving file `.travis.yml` to  `.travis.yml.pre_hp4t`"
   mv $HP4T_PROJECT_DIR/.travis.yml $HP4T_PROJECT_DIR/.travis.yml.pre_hp4t
 fi
-cp $HP4T_TEMPLATES_DIR/travis/npm/.travis.yml $HP4T_PROJECT_DIR/.travis.yml
+cp $HP4T_TEMPLATES_DIR/travis/$projectType/.travis.yml $HP4T_PROJECT_DIR/.travis.yml
