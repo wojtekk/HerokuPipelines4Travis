f4460973dea102a73490d78ea493aefad8749404
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2016-03-21 14:35:01 +0100

    Move NodeJS Infrastructure templates to subdirectory

--- scripts/initialise-infrastructure.sh
+++ scripts/initialise-infrastructure.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ ! -d $HP4T_PROJECT_DIR/infrastructure ]; then
   echo "[HP4T] Infrastructure configration: initialising"
-  cp -r $HP4T_TEMPLATES_DIR/infrastructure $HP4T_PROJECT_DIR/infrastructure
+  cp -r $HP4T_TEMPLATES_DIR/infrastructure/npm $HP4T_PROJECT_DIR/infrastructure
 else
   echo "[HP4T] Infrastructure configration: exists - skipping"
 fi
