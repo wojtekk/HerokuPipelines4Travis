85778a5d9a8b3693d2a54dba72701f447ba38c95
Wojciech Krawczyk <wojtekk@gmail.com>
2016-05-04 22:32:41 +0200

    Validate HEROKU_API_KEY before provisioning

--- src/provision.js
+++ src/provision.js
@@ -6,6 +6,10 @@ if( !process.argv[2] ) {
   console.error('Config file does not exists');
 }
 
+if( !process.env.HEROKU_API_KEY ) {
+  throw new Error('Please set env var for HEROKU_API_KEY');
+}
+
 let configFile = process.argv[2];
 console.log(configFile);
 let configuration = require(configFile);
