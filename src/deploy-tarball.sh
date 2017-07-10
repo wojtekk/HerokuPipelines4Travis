3b291cb22cb02d8d3a4628dee8896e5563ebe5e2
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2017-07-10 15:34:29 +0200

    Do not read api key from arguments Fix command name

--- src/deploy-tarball.sh
+++ src/deploy-tarball.sh
@@ -3,6 +3,5 @@ set -e
 
 package=$1
 app=$2
-apikey=$3
 
-node $HP4T_EXTENSION_DIR/src/deploy-tarball.js $package $app --api-key=$apikey
+node $HP4T_EXTENSION_DIR/src/deploy-tarball.js $package $app --api-key=$HEROKU_API_KEY
