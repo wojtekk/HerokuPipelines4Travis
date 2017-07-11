1e951e18886dbd8062ede20a4d3c07778e697b24
Wojciech Krawczyk <wojciech.krawczyk@schibsted.com>
2017-07-11 11:53:22 +0200

    do yarn install before createing archive

--- src/package.sh
+++ src/package.sh
@@ -4,6 +4,9 @@ set -e
 output=$1
 
 $(npm bin)/bundle-deps
+if [ -f "yarn.lock" ]; then
+  yarn install --prefer-offline --no-progress # refresh checksume in yarn.lock - important for heroku
+fi
 PACKAGE=$(npm pack)
 
 $(npm bin)/tar-rename package "" -s $PACKAGE -o $output
